01e8f9a408ba1f9251b790ca78767b2ec67536cc
Your Name <you@example.com>
2016-11-23 13:12:19 -0300

    arreglando boton next apara que suba - prueba 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+  // CODIGO PARA EFECTOS INPUT
   $(document).ready(function() {
     $('select').material_select();
-  });
+  
+
+  // EFECTOS UP EN BOTON NEXT SIGN-UP.HTML
+
+  //$('#phono').on('click',function(){
+  	//$('.btn-large').toggleClass('.up');
+  //});
+
+
+  // SELECCIONAR BANDERA - AGREGAR CODIGO DE AREA
+
+
+  $('.icons').change(function(){
+ 	var country = $('.icons option:selected');
+ 	 $('#phone').html
+
+  })
+
+
+
+});
